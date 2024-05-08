4159b8c10bddc42291773734d5df8e858b21cdb1
kody.low <kodylow7@gmail.com>
2024-05-08 10:11:05 -0700

    feat: tokio spawn for concurrent redis client handling

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ mod server;
 use tracing::Level;
 use tracing_subscriber::fmt::Subscriber;
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let subscriber = Subscriber::builder().with_max_level(Level::INFO).finish();
     tracing::subscriber::set_global_default(subscriber)?;
 
     const REDIS_SERVER_ADDRESS: &str = "127.0.0.1:6379";
-    server::start_redis_server(REDIS_SERVER_ADDRESS)
+    server::start_redis_server(REDIS_SERVER_ADDRESS).await
 }
--- src/server.rs
+++ src/server.rs
@@ -1,45 +1,36 @@
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 use tracing::{error, info};
 
 use crate::redis::{RedisCommand, RedisCommandHandler};
 
-pub fn start_redis_server(address: &str) -> anyhow::Result<()> {
-    let listener = TcpListener::bind(address)?;
+pub async fn start_redis_server(address: &str) -> anyhow::Result<()> {
+    let listener = TcpListener::bind(address).await?;
     info!("Redis Server listening on {}", address);
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => handle_client(&mut stream)?,
-            Err(e) => {
-                error!("Connection failed: {}", e);
-                continue;
+    loop {
+        let (stream, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            if let Err(e) = handle_client(stream).await {
+                error!("Error handling client: {}", e);
             }
-        }
+        });
     }
-    Ok(())
 }
 
-fn handle_client(stream: &mut TcpStream) -> Result<(), anyhow::Error> {
-    let mut buffer = [0; 1024];
+async fn handle_client(mut stream: TcpStream) -> anyhow::Result<()> {
+    let mut buffer = vec![0; 1024];
     loop {
-        match stream.read(&mut buffer) {
-            Ok(0) => {
-                break;
-            }
-            Ok(_) => {
-                let command = RedisCommand::from_buffer(&buffer)?;
-                info!("Received command: {}", command);
-                let response = RedisCommandHandler::handle_command(command);
-                stream.write_all(response.message.as_bytes())?;
-                info!("Sent response: {}", response.message);
-                buffer.fill(0);
-            }
-            Err(e) => {
-                error!("Failed to read from connection: {}", e);
-                break;
-            }
+        let n = stream.read(&mut buffer).await?;
+        if n == 0 {
+            break;
         }
+        let command = RedisCommand::from_buffer(&buffer)?;
+        info!("Received command: {}", command);
+        let response = RedisCommandHandler::handle_command(command);
+        stream.write_all(response.message.as_bytes()).await?;
+        info!("Sent response: {}", response.message);
+        buffer.fill(0);
     }
     Ok(())
 }
